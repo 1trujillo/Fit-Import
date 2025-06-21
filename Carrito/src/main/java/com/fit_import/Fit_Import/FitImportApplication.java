cd3b53d6e65154e1490c90be2b25ea2d141cac3d
0trujillo <yojanseldelacruz4@gmail.com>
2025-06-20 23:37:59 -0400

    Servicio carrito final

---
+++ Carrito/src/main/java/com/fit_import/Fit_Import/FitImportApplication.java
@@ -0,0 +1,13 @@
+package com.fit_import.Fit_Import;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitImportApplication.class, args);
+	}
+
+}
